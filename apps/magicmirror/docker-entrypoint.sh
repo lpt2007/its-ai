903a14dd5cebec6229f00741b8a0e485e773a193
lpt2007 <38924279+lpt2007@users.noreply.github.com>
2019-02-16 20:39:19 +0100

    Create docker-entrypoint.sh

---
+++ apps/magicmirror/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -f /opt/magic_mirror/modules ]; then
+    cp -Rn /opt/default_modules/. /opt/magic_mirror/modules
+fi
+
+if [ ! -f /opt/magic_mirror/config ]; then
+    cp -Rn /opt/default_config/. /opt/magic_mirror/config
+fi
+
+$@
