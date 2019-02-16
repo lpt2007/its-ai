633633f9c9d2984c8ef62c38a8f276520943a0a2
lpt2007 <38924279+lpt2007@users.noreply.github.com>
2019-02-16 21:01:17 +0100

    Update its-ai.sh

--- its-ai.sh
+++ its-ai.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-# if not root, run as root
-if (( $EUID != 0 )); then
-    sudo /home/jb/bash/update.sh
-    exit
-fi
-apt update
-apt -y dist-upgrade
-apt -y autoremove
-apt clean
-apt purge -y $(dpkg -l | awk '/^rc/ { print $2 }')
+sudo apt update
+sudo apt -y dist-upgrade
+sudo apt -y autoremove
+sudo apt clean
+sudo apt purge -y $(dpkg -l | awk '/^rc/ { print $2 }')
