f6ac4433bf2826ef79ebce6d9ef6a879817a610a
lpt2007 <38924279+lpt2007@users.noreply.github.com>
2019-02-16 21:16:51 +0100

    Update its-ai.sh

--- its-ai.sh
+++ its-ai.sh
@@ -37,8 +37,25 @@ mylog 2 "fix and attempt to correct a system with broken dependencies"
 sudo apt-get install -f
 
 # step 3
-mylog 3 "the end"
-echo baz
+mylog 3 "update apt cache"
+sudo apt-get update
+
+# step 4
+mylog 4 "upgrade packages"
+sudo apt-get upgrade
+
+# step 5
+mylog 5 "distribution upgrade"
+sudo apt-get dist-upgrade
+
+# step 6
+mylog 6 "remove unused packages"
+sudo apt-get --purge autoremove
+
+# step 7
+mylog 7 "clean up"
+sudo apt-get autoclean
+
 if [[ $? == 0 ]]; then
         myfail 3 "nothing really"
 fi
