e56cd56a8c82bd4a5b4d5968482c07479a4787ac
lpt2007 <38924279+lpt2007@users.noreply.github.com>
2019-03-26 18:44:14 +0100

    Update launch_app.sh

--- apps/kodi/src/launch_app.sh
+++ apps/kodi/src/launch_app.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-# Disable DPMS / Screen blanking
-#xset -dpms
-#xset s off
-#xset s noblank
-
-# Change Keyboard Layout from US to German
-#setxkbmap de
-
-# Debug Tools
-#xinput --list
-#evtest
-
-# Start Window Manager
-#sudo matchbox-window-manager -use_cursor yes -use_titlebar no &
-#sudo matchbox-window-manager -use_cursor no -use_titlebar no &
-
-# Start Payload App
-#xterm
 kodi
