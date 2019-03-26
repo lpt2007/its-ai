cedcdec08b5cc81f235067f5dd65d3c3dcf5b547
lpt2007 <38924279+lpt2007@users.noreply.github.com>
2019-03-26 14:45:45 +0100

    Update launch_app.sh

--- apps/kodi/src/launch_app.sh
+++ apps/kodi/src/launch_app.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Disable DPMS / Screen blanking
-xset -dpms
-xset s off
-xset s noblank
+#xset -dpms
+#xset s off
+#xset s noblank
 
 # Change Keyboard Layout from US to German
-setxkbmap de
+#setxkbmap de
 
 # Debug Tools
 #xinput --list
 #evtest
 
 # Start Window Manager
-sudo matchbox-window-manager -use_cursor yes -use_titlebar no &
+#sudo matchbox-window-manager -use_cursor yes -use_titlebar no &
 #sudo matchbox-window-manager -use_cursor no -use_titlebar no &
 
 # Start Payload App
